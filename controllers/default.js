5876c42e0b6c00daa442de5ddb1af1c2021e70c3
Sangram Mohite <mohite.sangram@gmail.com>
2019-09-20 19:32:00 +0530

    Added ORM and List of all APIs that need to be implemented.

--- controllers/default.js
+++ controllers/default.js
@@ -1,6 +1,6 @@
 const defaultController = {
     sendResponse(request, response) {
-        console.log(`Sending default response for: ${request.originalUrl}`);
+        console.log(`Sending default response for: "${request.originalUrl}" Method:${request.method}`);
         response.status(404).send({
             message: "This is the default response to the API. It has not been yet implemented.",
         })
