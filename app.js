262ff036232b701b035948bba5e5b15a476b4a89
Sangram Mohite <mohite.sangram@gmail.com>
2019-09-20 16:42:02 +0530

    CORS Support

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true,
 }));
-
+app.use(function(req, res, next) {
+    console.log("Adding CORS headers");
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 routes(app);
 
 // Start the server
